402b0d32bacebab7826f1e5bb361af5722107668
avc <avc@avc.home>
2017-11-05 20:31:59 +0100

    some fields in setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='',
+      version='1.0',
+      description='',
+      author='Dawid Galus',
+      author_email='dawid_galus@wp.pl',
+      url='https://github.com/dgalus/msc_thesis',
+      packages=[],
+     )

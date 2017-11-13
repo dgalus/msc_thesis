42e3324596774f1893ffbf0ec8893bbe80a59985
avc <dawid_galus@wp.pl>
2017-11-13 23:57:21 +0100

    connection to elasticsearch

--- app/data_fetcher/__init__.py
+++ app/data_fetcher/__init__.py
@@ -2,6 +2,7 @@ from . import malc0de
 from . import malwaredomainlist
 from . import openphish
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 import itertools
 
 class DataFetcher:
@@ -12,6 +13,10 @@ class DataFetcher:
 
     def insert_unsafe_urls(self):
         unsafe_urls = list(itertools.chain(malwaredomainlist.get_unsafe_domains(), openphish.get_unsafe_urls()))
+        for uu in unsafe_urls:
+            self.es.index(index='unsafe', doc_type='urls', body={
+                'url' : uu
+            })
         
 
     def insert_unsafe_addresses(self):
--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import yaml
+from elasticsearch import Elasticsearch
 from app.data_fetcher import DataFetcher
 
 def read_config():
     with open("config.yml", 'r') as stream:
         try:
-            print(yaml.load(stream))
+            return yaml.load(stream)
         except yaml.YAMLError as exc:
             print(exc)
 
-df = DataFetcher(None)
+config = read_config()
+es = Elasticsearch([{'host' : config['elasticsearch']['host'], 'port' : config['elasticsearch']['port']}])
+
+df = DataFetcher(es)
 df.insert_unsafe_urls()

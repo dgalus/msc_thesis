76a919ad9f815abcc9d53b51631cd9920da7f8fd
avc <dawid_galus@wp.pl>
2017-11-21 23:28:11 +0100

    threaded sniff

--- app/pcap_analysis/capture.py
+++ app/pcap_analysis/capture.py
@@ -1,5 +1,28 @@
+#from scapy.all import *
+
+#packets = sniff(count=100)
+#wrpcap('traffic.pcap', packets)
+
+from threading import Thread
+from queue import Queue, Empty
 from scapy.all import *
 
-packets = sniff(count=100)
-wrpcap('traffic.pcap', packets)
+def print_summary(pkt):
+    print(pkt.summary())
 
+def threaded_sniff_target(q):
+    sniff(prn = lambda x : q.put(x))
+
+def threaded_sniff():
+    q = Queue()
+    sniffer = Thread(target = threaded_sniff_target, args = (q,))
+    sniffer.daemon = True
+    sniffer.start()
+    while(True):
+        try:
+            pkt = q.get(timeout=1)
+            print_summary(pkt)
+        except Empty:
+            pass
+
+#threaded_sniff()

482eed777e95baaf3be4c6d40d306095e136fe24
avc <avc@localhost.localdomain>
2017-12-02 10:30:58 +0100

    constructor, getters and setters for elasticsearch

--- real_time/elasticsearch.cpp
+++ real_time/elasticsearch.cpp
@@ -1,1 +1,48 @@
 #include "elasticsearch.h"
+
+Elasticsearch::Elasticsearch()
+{
+	this->port = 9200;
+	this->host = "127.0.0.1";
+	this->username = "";
+	this->password = "";
+}
+
+Elasticsearch::Elasticsearch(std::string host, uint16_t port)
+{
+	this->port = port;
+	this->host = host;
+	this->username = "";
+	this->password = "";
+}
+
+Elasticsearch::Elasticsearch(std::string host, std::string username, std::string password, uint16_t port)
+{
+	this->host = host;
+	this->username = username;
+	this->password = password;
+	this->port = port;
+}
+
+void Elasticsearch::SetHost(std::string host)
+{
+	this->host = host;
+}
+
+void Elasticsearch::SetPassword(std::string password)
+{
+	this->password = password;
+}
+
+void Elasticsearch::SetPort(uint16_t port)
+{
+	this->port = port;
+}
+
+void Elasticsearch::SetUsername(std::string username)
+{
+	this->username = username;
+}
+
+
+
